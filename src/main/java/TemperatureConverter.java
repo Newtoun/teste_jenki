e30daa6192f50975ae4619c1c45217def87063c5
Nilton <101841984+Newtoun@users.noreply.github.com>
2024-07-19 19:31:19 -0500

    t

---
+++ src/main/java/TemperatureConverter.java
@@ -0,0 +1,10 @@
+package src.main.java;
+public class TemperatureConverter {
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5.0/9.0;
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9.0/5.0) + 32;
+    }
+}
---
+++ test/java/TemperatureConverterTest.java
@@ -0,0 +1,19 @@
+package test.java;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import src.main.java.TemperatureConverter;
+
+public class TemperatureConverterTest {
+    @Test
+    public void testFahrenheitToCelsius() {
+        assertEquals(0, TemperatureConverter.fahrenheitToCelsius(32), 0.001);
+        assertEquals(100, TemperatureConverter.fahrenheitToCelsius(212), 0.001);
+    }
+
+    @Test
+    public void testCelsiusToFahrenheit() {
+        assertEquals(32, TemperatureConverter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(212, TemperatureConverter.celsiusToFahrenheit(100), 0.001);
+    }
+}
