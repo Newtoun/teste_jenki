dee6279e323f8156c961ec3664d14b276480439d
Nilton <101841984+Newtoun@users.noreply.github.com>
2024-07-19 23:00:01 -0300

    Erro no codigo teste

--- test/java/TemperatureConverterTest.java
+++ test/java/TemperatureConverterTest.java
@@ -13,7 +13,7 @@ public class TemperatureConverterTest {
 
     @Test
     public void testCelsiusToFahrenheit() {
-        assertEquals(32, TemperatureConverter.celsiusToFahrenheit(0), 0.001);
+        assertEquals(30, TemperatureConverter.celsiusToFahrenheit(0), 0.001);
         assertEquals(212, TemperatureConverter.celsiusToFahrenheit(100), 0.001);
     }
 }
